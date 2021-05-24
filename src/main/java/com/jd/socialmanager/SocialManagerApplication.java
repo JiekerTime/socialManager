343643b394af2ff26ba9d34297a78aa4a81b5db3
JiekerTime <jieker_mail@163.com>
2021-05-24 17:15:48 +0800

    problem fixed

--- src/main/java/com/jd/socialmanager/SocialManagerApplication.java
+++ src/main/java/com/jd/socialmanager/SocialManagerApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.wdd.studentmanager.dao")
+@MapperScan("com.jd.socialmanager.dao")
 @SpringBootApplication()
 public class SocialManagerApplication {
 
